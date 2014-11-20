728664caf070e1d4ab5ee42be4825fa1a033fa03
JoaoAlexandre <joao.contact@icloud.com>
2014-11-20 09:32:19 -0200

    Adds Styles Imports CSV with UPCASE

--- app/models/parada.rb
+++ app/models/parada.rb
@@ -2,7 +2,10 @@ class Parada < ActiveRecord::Base
 	 
 	def self.import(file)
 	  CSV.foreach(file.path, headers: :true) do |row|
-	  	Parada.create! row.to_hash
+	  	row['endereco'].upcase!
+	  	row['bairro'].upcase! unless row['bairro'].nil?
+	  	hash = row.to_hash
+	  	Parada.create! hash
 	  end
 	end
 end
